33cf29b80cb6df5960c13f76527763bf562b90e4
Kristian <56667566+kristian821@users.noreply.github.com>
2021-04-19 20:58:09 -0700

    Added MySQL requests

--- server.js
+++ server.js
@@ -25,6 +25,34 @@ db.query(`SELECT * FROM candidates`, (err, rows) => {
     console.log(rows);
 });
 
+// GET a single candidate
+db.query(`SELECT * FROM candidates WHERE id = 1`, (err, rows) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(rows);
+});
+
+// DELETE a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+    VALUES (?,?,?,?)`;
+const params = [1, 'Ronald', 'Firbank', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err)
+    }
+    console.log(result);
+});
+
 // Default response for any other request(Not Found)
 app.get((req, res) => {
     res.status(404).end();
